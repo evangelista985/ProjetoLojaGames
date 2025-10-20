20c3d59e48a2d0f70d93f740d0946a31c5efbca1
Fabio Evangelista <evangelistafabio985@gmail.com>
2025-10-20 20:34:44 -0200

    Atualização de Tela

--- LojaGames/Pedido.Designer.cs
+++ LojaGames/Pedido.Designer.cs
@@ -28,135 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.lblValorOpcionais = new System.Windows.Forms.Label();
+            this.lblValorPagar = new System.Windows.Forms.Label();
+            this.lblValorJogo = new System.Windows.Forms.Label();
+            this.cmbTiposJogos = new System.Windows.Forms.ComboBox();
+            this.txtValorJogo = new System.Windows.Forms.TextBox();
+            this.txtValorOpcionais = new System.Windows.Forms.TextBox();
+            this.grpOpcionais = new System.Windows.Forms.GroupBox();
+            this.chk2Controles = new System.Windows.Forms.CheckBox();
+            this.chkTesteDriver = new System.Windows.Forms.CheckBox();
+            this.chkTotalPass = new System.Windows.Forms.CheckBox();
+            this.chk2Contas = new System.Windows.Forms.CheckBox();
+            this.txtValorPagar = new System.Windows.Forms.TextBox();
+            this.grpOpcionais.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // lblValorOpcionais
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(371, 74);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblValorOpcionais.AutoSize = true;
+            this.lblValorOpcionais.Location = new System.Drawing.Point(351, 34);
+            this.lblValorOpcionais.Name = "lblValorOpcionais";
+            this.lblValorOpcionais.Size = new System.Drawing.Size(130, 13);
+            this.lblValorOpcionais.TabIndex = 0;
+            this.lblValorOpcionais.Text = "VALOR DOS OPCIONAIS";
             // 
-            // label2
+            // lblValorPagar
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(441, 74);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.lblValorPagar.AutoSize = true;
+            this.lblValorPagar.Location = new System.Drawing.Point(529, 34);
+            this.lblValorPagar.Name = "lblValorPagar";
+            this.lblValorPagar.Size = new System.Drawing.Size(93, 13);
+            this.lblValorPagar.TabIndex = 1;
+            this.lblValorPagar.Text = "VALOR A PAGAR";
             // 
-            // label3
+            // lblValorJogo
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(314, 74);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.lblValorJogo.AutoSize = true;
+            this.lblValorJogo.Location = new System.Drawing.Point(236, 29);
+            this.lblValorJogo.Name = "lblValorJogo";
+            this.lblValorJogo.Size = new System.Drawing.Size(94, 13);
+            this.lblValorJogo.TabIndex = 2;
+            this.lblValorJogo.Text = "VALOR DO JOGO";
             // 
-            // comboBox1
+            // cmbTiposJogos
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(355, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 3;
+            this.cmbTiposJogos.FormattingEnabled = true;
+            this.cmbTiposJogos.Location = new System.Drawing.Point(33, 26);
+            this.cmbTiposJogos.Name = "cmbTiposJogos";
+            this.cmbTiposJogos.Size = new System.Drawing.Size(121, 21);
+            this.cmbTiposJogos.TabIndex = 3;
+            this.cmbTiposJogos.Text = "TIPOS DE JOGOS";
             // 
-            // textBox1
+            // txtValorJogo
             // 
-            this.textBox1.Location = new System.Drawing.Point(306, 118);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 4;
+            this.txtValorJogo.Location = new System.Drawing.Point(239, 64);
+            this.txtValorJogo.Name = "txtValorJogo";
+            this.txtValorJogo.Size = new System.Drawing.Size(100, 20);
+            this.txtValorJogo.TabIndex = 4;
             // 
-            // textBox2
+            // txtValorOpcionais
             // 
-            this.textBox2.Location = new System.Drawing.Point(444, 118);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 5;
+            this.txtValorOpcionais.Location = new System.Drawing.Point(381, 64);
+            this.txtValorOpcionais.Name = "txtValorOpcionais";
+            this.txtValorOpcionais.Size = new System.Drawing.Size(100, 20);
+            this.txtValorOpcionais.TabIndex = 5;
             // 
-            // groupBox1
+            // grpOpcionais
             // 
-            this.groupBox1.Location = new System.Drawing.Point(22, 156);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 6;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.grpOpcionais.Controls.Add(this.chk2Contas);
+            this.grpOpcionais.Controls.Add(this.chkTesteDriver);
+            this.grpOpcionais.Controls.Add(this.chkTotalPass);
+            this.grpOpcionais.Controls.Add(this.chk2Controles);
+            this.grpOpcionais.Location = new System.Drawing.Point(22, 156);
+            this.grpOpcionais.Name = "grpOpcionais";
+            this.grpOpcionais.Size = new System.Drawing.Size(200, 123);
+            this.grpOpcionais.TabIndex = 6;
+            this.grpOpcionais.TabStop = false;
+            this.grpOpcionais.Text = "ESCOLHA OPCIONAIS";
             // 
-            // checkBox1
+            // chk2Controles
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(280, 156);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 7;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.chk2Controles.AutoSize = true;
+            this.chk2Controles.Location = new System.Drawing.Point(11, 42);
+            this.chk2Controles.Name = "chk2Controles";
+            this.chk2Controles.Size = new System.Drawing.Size(101, 17);
+            this.chk2Controles.TabIndex = 7;
+            this.chk2Controles.Text = "2 CONTROLES";
+            this.chk2Controles.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // chkTesteDriver
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(269, 239);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(80, 17);
-            this.checkBox2.TabIndex = 8;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.chkTesteDriver.AutoSize = true;
+            this.chkTesteDriver.Location = new System.Drawing.Point(11, 88);
+            this.chkTesteDriver.Name = "chkTesteDriver";
+            this.chkTesteDriver.Size = new System.Drawing.Size(105, 17);
+            this.chkTesteDriver.TabIndex = 8;
+            this.chkTesteDriver.Text = "TESTE DRIVER";
+            this.chkTesteDriver.UseVisualStyleBackColor = true;
             // 
-            // checkBox3
+            // chkTotalPass
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(250, 202);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(80, 17);
-            this.checkBox3.TabIndex = 9;
-            this.checkBox3.Text = "checkBox3";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.chkTotalPass.AutoSize = true;
+            this.chkTotalPass.Location = new System.Drawing.Point(11, 65);
+            this.chkTotalPass.Name = "chkTotalPass";
+            this.chkTotalPass.Size = new System.Drawing.Size(134, 17);
+            this.chkTotalPass.TabIndex = 9;
+            this.chkTotalPass.Text = "TOTAL PASS GAMER";
+            this.chkTotalPass.UseVisualStyleBackColor = true;
             // 
-            // checkBox4
+            // chk2Contas
             // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(269, 179);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(80, 17);
-            this.checkBox4.TabIndex = 10;
-            this.checkBox4.Text = "checkBox4";
-            this.checkBox4.UseVisualStyleBackColor = true;
+            this.chk2Contas.AutoSize = true;
+            this.chk2Contas.Location = new System.Drawing.Point(11, 19);
+            this.chk2Contas.Name = "chk2Contas";
+            this.chk2Contas.Size = new System.Drawing.Size(79, 17);
+            this.chk2Contas.TabIndex = 10;
+            this.chk2Contas.Text = "2 CONTAS";
+            this.chk2Contas.UseVisualStyleBackColor = true;
+            // 
+            // txtValorPagar
+            // 
+            this.txtValorPagar.Location = new System.Drawing.Point(522, 64);
+            this.txtValorPagar.Name = "txtValorPagar";
+            this.txtValorPagar.Size = new System.Drawing.Size(100, 20);
+            this.txtValorPagar.TabIndex = 11;
             // 
             // Pedido
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtValorPagar);
+            this.Controls.Add(this.grpOpcionais);
+            this.Controls.Add(this.txtValorOpcionais);
+            this.Controls.Add(this.txtValorJogo);
+            this.Controls.Add(this.cmbTiposJogos);
+            this.Controls.Add(this.lblValorJogo);
+            this.Controls.Add(this.lblValorPagar);
+            this.Controls.Add(this.lblValorOpcionais);
             this.Name = "Pedido";
             this.Text = "Form2";
+            this.grpOpcionais.ResumeLayout(false);
+            this.grpOpcionais.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -164,16 +177,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label lblValorOpcionais;
+        private System.Windows.Forms.Label lblValorPagar;
+        private System.Windows.Forms.Label lblValorJogo;
+        private System.Windows.Forms.ComboBox cmbTiposJogos;
+        private System.Windows.Forms.TextBox txtValorJogo;
+        private System.Windows.Forms.TextBox txtValorOpcionais;
+        private System.Windows.Forms.GroupBox grpOpcionais;
+        private System.Windows.Forms.CheckBox chk2Controles;
+        private System.Windows.Forms.CheckBox chkTesteDriver;
+        private System.Windows.Forms.CheckBox chkTotalPass;
+        private System.Windows.Forms.CheckBox chk2Contas;
+        private System.Windows.Forms.TextBox txtValorPagar;
     }
 }
